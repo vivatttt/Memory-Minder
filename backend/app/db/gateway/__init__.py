63dbb9ed4187c109d4ac6b79c4c80e30bd71df52
Душейко Дарья Васильевна <dvdusheiko@edu.hse.ru>
2025-01-11 22:55:04 +0300

    add_second_game

--- backend/app/db/gateway/__init__.py
+++ backend/app/db/gateway/__init__.py
@@ -1,7 +1,14 @@
 from backend.app.db.gateway.false_state_stats import FalseStateStatsGateway
-from backend.app.db.gateway.user import UserGateway
+from backend.app.db.gateway.names_memory_stat import ImageMemoryStatGateway
+from backend.app.db.gateway.image import ImageGateway
+from backend.app.db.gateway.viewed_image import ViewedImageGateway
+from backend.app.db.gateway.user import UserGatewaywq
+
 
 __all__ = [
     "UserGateway",
     "FalseStateStatsGateway",
+    "ImageMemoryStatGateway",
+    "ImageGateway",
+    "ViewedImageGateway",
 ]
